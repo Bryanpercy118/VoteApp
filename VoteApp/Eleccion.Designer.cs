058aab2617a406698bf3ab930d2b8541df7107f5
isatkm21 <isabellamanjarres67@gmail.com>
2023-11-27 09:51:18 -0500

    Update Eleccion

--- VoteApp/Eleccion.Designer.cs
+++ VoteApp/Eleccion.Designer.cs
@@ -29,40 +29,59 @@
         private void InitializeComponent()
         {
             this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label5 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.btnRegistrarCandidato = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
             this.panel2.SuspendLayout();
-            this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel2.Controls.Add(this.panel6);
+            this.panel2.Controls.Add(this.panel5);
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Controls.Add(this.panel3);
             this.panel2.Controls.Add(this.dataGridView1);
             this.panel2.Controls.Add(this.label5);
             this.panel2.Controls.Add(this.label2);
             this.panel2.Controls.Add(this.label3);
             this.panel2.Controls.Add(this.btnRegistrarCandidato);
-            this.panel2.Location = new System.Drawing.Point(-4, 52);
+            this.panel2.Location = new System.Drawing.Point(-3, 42);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(945, 544);
+            this.panel2.Size = new System.Drawing.Size(709, 442);
             this.panel2.TabIndex = 7;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 131);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(674, 219);
+            this.dataGridView1.TabIndex = 18;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Nirmala UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label5.Location = new System.Drawing.Point(17, 105);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(13, 85);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(294, 20);
+            this.label5.Size = new System.Drawing.Size(236, 15);
             this.label5.TabIndex = 17;
             this.label5.Text = "Seleccione al candidato de su preferencia";
             // 
@@ -71,9 +90,10 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Perpetua Titling MT", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.MenuBar;
-            this.label2.Location = new System.Drawing.Point(350, 15);
+            this.label2.Location = new System.Drawing.Point(262, 12);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(230, 28);
+            this.label2.Size = new System.Drawing.Size(185, 23);
             this.label2.TabIndex = 1;
             this.label2.Text = "E L E C C I O N E S";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -83,10 +103,9 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Nirmala UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label3.Location = new System.Drawing.Point(222, 161);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(166, 131);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(0, 20);
+            this.label3.Size = new System.Drawing.Size(0, 15);
             this.label3.TabIndex = 12;
             // 
             // btnRegistrarCandidato
@@ -97,9 +116,10 @@
             this.btnRegistrarCandidato.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnRegistrarCandidato.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnRegistrarCandidato.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.btnRegistrarCandidato.Location = new System.Drawing.Point(348, 465);
+            this.btnRegistrarCandidato.Location = new System.Drawing.Point(261, 378);
+            this.btnRegistrarCandidato.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnRegistrarCandidato.Name = "btnRegistrarCandidato";
-            this.btnRegistrarCandidato.Size = new System.Drawing.Size(232, 33);
+            this.btnRegistrarCandidato.Size = new System.Drawing.Size(174, 27);
             this.btnRegistrarCandidato.TabIndex = 0;
             this.btnRegistrarCandidato.Text = "Votar";
             this.btnRegistrarCandidato.UseVisualStyleBackColor = false;
@@ -109,8 +129,9 @@
             this.panel1.BackColor = System.Drawing.Color.LightBlue;
             this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(-1, -2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(942, 66);
+            this.panel1.Size = new System.Drawing.Size(706, 54);
             this.panel1.TabIndex = 6;
             // 
             // label1
@@ -118,36 +139,56 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Perpetua Titling MT", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.MenuText;
-            this.label1.Location = new System.Drawing.Point(347, 11);
+            this.label1.Location = new System.Drawing.Point(260, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(173, 28);
+            this.label1.Size = new System.Drawing.Size(139, 23);
             this.label1.TabIndex = 0;
             this.label1.Text = "V o t e A p p";
             // 
-            // dataGridView1
+            // panel3
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(21, 161);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(899, 269);
-            this.dataGridView1.TabIndex = 18;
+            this.panel3.Location = new System.Drawing.Point(33, 150);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(133, 176);
+            this.panel3.TabIndex = 19;
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(201, 150);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(135, 171);
+            this.panel4.TabIndex = 20;
+            // 
+            // panel5
+            // 
+            this.panel5.Location = new System.Drawing.Point(369, 150);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(137, 171);
+            this.panel5.TabIndex = 21;
+            // 
+            // panel6
+            // 
+            this.panel6.Location = new System.Drawing.Point(535, 150);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(139, 171);
+            this.panel6.TabIndex = 22;
             // 
             // Eleccion
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(939, 573);
+            this.ClientSize = new System.Drawing.Size(704, 466);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Eleccion";
             this.Text = "Eleccion";
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -162,5 +203,9 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel4;
     }
 }
